b10b197d3f6ea3e85a553a002804753f47959b98
Brooolly <a_noel@outlook.fr>
2022-04-24 14:13:13 +0200

    Update

--- src/js/script.js
+++ src/js/script.js
@@ -97,7 +97,7 @@ class Scene {
     this.video = video;
     
     video.setAttribute( 'crossOrigin','Anonymous',);
-    video.src = "https://vod-progressive.akamaized.net/exp=1650798302~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F503%2F28%2F702515198%2F3251801369.mp4~hmac=a82106ca343caae99514343fd773ea135a18a616682a66cb706033aaa36aeb44/vimeo-prod-skyfire-std-us/01/503/28/702515198/3251801369.mp4";
+    video.src = "https://vod-progressive.akamaized.net/exp=1650805855~acl=%2Fvimeo-prod-skyfire-std-us%2F01%2F503%2F28%2F702515198%2F3251804335.mp4~hmac=8d64ea6fe5a34108118a88ff08ee2932f2f59d5373956acb92536824498ab674/vimeo-prod-skyfire-std-us/01/503/28/702515198/3251804335.mp4";
     video.controls = 'autoplay',
     video.name = 'media',
     video.type = "video/mp4",
